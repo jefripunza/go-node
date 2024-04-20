85f76291db14b0dad3dcf6a84a11968c990ee8c1
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2024-04-20 19:19:31 +0700

    fix something

--- node_test.go
+++ node_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Joshua J Baker. All rights reserved.
+// Copyright 2024 Jefri Herdi Triyanto. All rights reserved.
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
